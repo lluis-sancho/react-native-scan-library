1cc241c67f193ffd4e1b75a0d3427397c797747f
Carlos Muiño Migallon <camumino@gmail.com>
2020-03-05 12:44:44 +0100

    added on read bar code method

--- android/src/main/java/com/reactlibrary/ScanLibraryModule.java
+++ android/src/main/java/com/reactlibrary/ScanLibraryModule.java
@@ -1,17 +1,27 @@
 package com.reactlibrary;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
-public class ScanLibraryModule extends ReactContextBaseJavaModule {
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
+public class ScanLibraryModule extends ReactContextBaseJavaModule {
+    public static final String m_Broadcastname = "com.barcode.sendBroadcast";
     private final ReactApplicationContext reactContext;
 
     public ScanLibraryModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(m_Broadcastname);
+        reactContext.registerReceiver(receiver, intentFilter);
     }
 
     @Override
@@ -19,9 +29,23 @@ public class ScanLibraryModule extends ReactContextBaseJavaModule {
         return "ScanLibrary";
     }
 
-    @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    void sendEvent(String code) {
+      if (this.reactContext.hasActiveCatalystInstance()) {
+          this.reactContext
+                  .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                  .emit("onRead", code);
+      }
     }
+
+    BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context arg0, Intent arg1) {
+          if (arg1.getAction().equals(m_Broadcastname)) {
+            String str = arg1.getStringExtra("BARCODE");
+            if (!"".equals(str)) {
+              sendEvent(str);
+            }
+          }
+        }
+    };
 }
