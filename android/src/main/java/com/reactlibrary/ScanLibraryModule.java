d5ca47e6fb6b2ed44ac007f39f2b35b21abab9c6
Carlos Muiño Migallon <camumino@gmail.com>
2020-07-09 18:09:59 +0200

    fix

--- android/src/main/java/com/reactlibrary/ScanLibraryModule.java
+++ android/src/main/java/com/reactlibrary/ScanLibraryModule.java
@@ -9,6 +9,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.uimanager.IllegalViewOperationException;
 
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
